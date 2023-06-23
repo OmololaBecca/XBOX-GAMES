9e8ceefa6227b58784d53103826545c064aa7017
OmololaBecca <folarebecca03@gmail.com>
2023-06-23 20:34:16 +0100

    changes for xbox project

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const copyrightFooter = document.getElementById("copyright-footer")
+const preloader = document.getElementById("preloader")
+
+
+
+setTimeout(function preloaderTimeOut (){
+    preloader.style = "display: none"
+}, 5000)
 
 copyrightFooter.innerHTML = "© " + new Date().getFullYear() + " eXStore"
